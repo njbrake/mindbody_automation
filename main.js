3ad9b973ca753ae13269e5a9c85132d483f0d140
Nate Brake <njbrake@gmail.com>
2020-07-19 00:26:57 -0400

    tested and working

--- main.js
+++ main.js
@@ -3,7 +3,11 @@ const schedule = require("node-schedule");
 const mailgun = require("mailgun-js");
 require("dotenv").config();
 
+// how long to wait between each action so page can load
 const delay = 3000;
+// Should this be on a evening timer or are you running it by default?
+const manualRun = false;
+
 async function email(message) {
   const DOMAIN = "sandbox123d47fcdcf2427b91065072f46d6d79.mailgun.org";
   const mg = mailgun({
@@ -13,17 +17,17 @@ async function email(message) {
   const data = {
     from: process.env.MAILGUN_ENDPOINT,
     to: process.env.EMAIL,
-    subject: "Registration Failed",
+    subject: message,
     text: message,
   };
   mg.messages().send(data, function (error, body) {
     console.log(body);
   });
 }
+
 async function register(page) {
   await page.waitFor(delay);
   let date = new Date();
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun", "Mon"];
   let whatIWantThatDay = [
     `//*[@id="classSchedule-mainTable"]/tbody/tr[170]/td[2]/input`, // Sunday at 12:15,
     `//*[@id="classSchedule-mainTable"]/tbody/tr[5]/td[2]/input`, // Monday at 7:15
@@ -35,9 +39,9 @@ async function register(page) {
     `//*[@id="classSchedule-mainTable"]/tbody/tr[170]/td[2]/input`, // Sunday at 12:15,
     `//*[@id="classSchedule-mainTable"]/tbody/tr[5]/td[2]/input`, // Monday at 7:15
   ];
-  if (date.getDay() >= 5) {
+  if (date.getDay() >= 5 || date.getDay() === 0) {
     try {
-      page.click("#week-arrow-r"); //Click to get to the next weeks listings
+      await page.click("#week-arrow-r"); //Click to get to the next weeks listings
       await page.waitFor(100); //give it a second to make the change.
     } catch {
       await page.screenshot({ path: "cantFindNextWeek.png" }); //Document the page if it failed
@@ -45,6 +49,7 @@ async function register(page) {
       throw new Error("next week Button not found");
     }
   }
+  await page.waitFor(delay);
   const [button] = await page.$x(`${whatIWantThatDay[date.getDay() + 3]}`); // Register for 2 days in the future
   if (button) {
     await button.click();
@@ -55,7 +60,7 @@ async function register(page) {
   }
   await page.waitFor(delay);
   try {
-    page.click("#SubmitEnroll2");
+    await page.click("#SubmitEnroll2");
   } catch {
     await page.screenshot({ path: "cantFindEnrollButton.png" }); //Document the page if it failed
     await email("enroll button not found");
@@ -78,38 +83,47 @@ async function login(browser) {
 
 async function main() {
   let browser;
-  try {
-    if (process.env.PLATFORM !== "rPi") {
-      browser = await puppeteer.launch({
-        headless: false,
-        defaultViewport: null,
-      });
-    } else {
-      browser = await puppeteer.launch({
-        headless: true,
-        defaultViewport: null,
-        executablePath: "chromium-browser",
-      });
-    }
-    //Every day at midnight
-    let j = schedule.scheduleJob("swimming", "0 0 * * *", async function () {
-      console.log("Time to book swimming!");
-      await email("Time to book swimming!");
-      const browser = await puppeteer.launch(chromeOptions);
-      const page = await login(browser);
-      await register(page);
-      page.waitFor(delay);
-      //await browser.close();
-      let job = schedule.scheduledJobs["swimming"];
-      console.log(job.nextInvocation());
+  if (process.env.PLATFORM !== "rPi") {
+    browser = await puppeteer.launch({
+      headless: false,
+      defaultViewport: null,
     });
-  } catch {
-    console.log("Something Errored Out");
-    await browser.close();
-    schedule.cancelJob();
+  } else {
+    browser = await puppeteer.launch({
+      headless: true,
+      defaultViewport: null,
+      executablePath: "chromium-browser",
+    });
   }
-  //id = SubmitEnroll2;
-  //*[@id="classSchedule-mainTable"]/tbody/tr[120]/td[2]/input
+  if (manualRun) {
+    console.log("Time to book swimming!");
+    const page = await login(browser);
+    await register(page);
+    page.waitFor(delay);
+    console.log("done");
+    //await browser.close();
+    let job = schedule.scheduledJobs["swimming"];
+    console.log("done");
+    console.log(job.nextInvocation());
+  } else {
+    try {
+      //Every day at midnight
+      let j = schedule.scheduleJob("swimming", "0 0 * * *", async function () {
+        console.log("Time to book swimming!");
+        await email("Time to book swimming!");
+        const page = await login(browser);
+        await register(page);
+        page.waitFor(delay);
+        //await browser.close();
+        let job = schedule.scheduledJobs["swimming"];
+        console.log(job.nextInvocation());
+      });
+    } catch {
+      console.log("Something Errored Out");
+      await browser.close();
+      schedule.cancelJob();
+    }
+  }
   return;
 }
 
